534237ba79767ef8891f01e59fac72539f4a79aa
wifituga <manuel.nazaret.zambrano.moran@gmail.com>
2024-05-30 23:31:09 -0500

    feat(frontend): modified error-page

--- src/scenes/errorPage/ErrorPage.jsx
+++ src/scenes/errorPage/ErrorPage.jsx
@@ -4,18 +4,21 @@ import './ErrorPage.css';
 export default function ErrorPage() {
     return (
         <div className='error-page'>
-            <div className='error-page__error'>
-                <h1 className='error-page__error__code'>404 </h1>
-                <p className='error-page__error__description'>Not Found</p>
+            <div className='error-page__content'>
+                <div className='error-page__error'>
+                    <h1 className='error-page__error__code'>oops!</h1>
+                </div>
+                <p className='error-page__message'>
+                    buk not faund <i>[sic]</i><sup className='error-page__number'> 404</sup>
+                </p>
+                <LinkButton
+                    to='/homePage'
+                    linkClassName='error-page__link-button__link'
+                    buttonClassName='error-page__link-button__button'
+                >
+                    Volver a la página principal
+                </LinkButton>
             </div>
-            <p className='error-page__message'>¡Ups! Parece que algo salió mal. Estamos trabajando para solucionarlo.</p>
-            <LinkButton
-                to='/homePage'
-                linkClassName='error-page__link-button__link'
-                buttonClassName='error-page__link-button__button'
-            >
-                Volver a la página principal
-            </LinkButton>
         </div>
     );
 }
