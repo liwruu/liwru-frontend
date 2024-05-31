a09a176cdd9fa6d43cdf335f72408f5371dc7317
wifituga <manuel.nazaret.zambrano.moran@gmail.com>
2024-05-31 10:53:20 -0500

    fix(frontend): fixed home-page path

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
         element: <Layout />,
         children: [
             {
-                path: '/homePage',
+                path: '/',
                 element: <HomePage />,
             },
             {
