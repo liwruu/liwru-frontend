c5137b5c7c0b78dd11e445e89ffcfed9a48292a0
antrozi <20193439@aloe.ulima.edu.pe>
2024-06-24 17:57:43 -0500

    Añado busqueda avanzada en pantalla search page

--- src/scenes/searchPage/main/SearchPage.jsx
+++ src/scenes/searchPage/main/SearchPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import SearchResult from '../SearchResult';
 import detail from '../../../assets/data/detail';
@@ -6,13 +6,37 @@ import './SearchPage.css';
 
 export default function SearchPage() {
     const [data, setData] = useState(detail[1]);
+    const [filteredData, setFilteredData] = useState(detail[1]);
+    const [filter, setFilter] = useState('');
     const params = useParams();
 
-    const filteredResults = data.filter(call => 
-        call.title.toLowerCase().includes(params.searchQuery.toLowerCase())
-    );
+    const filters = ['Clásicos de la Literatura', 'Más Solicitados'];
 
-    const results = filteredResults.map(call => (
+    useEffect(() => {
+        if (filter) {
+            const filteredResults = data.filter(call => 
+                call.type === filter && call.title.toLowerCase().includes(params.searchQuery.toLowerCase())
+            );
+            setFilteredData(filteredResults);
+        } else {
+            const filteredResults = data.filter(call => 
+                call.title.toLowerCase().includes(params.searchQuery.toLowerCase())
+            );
+            setFilteredData(filteredResults);
+        }
+    }, [filter, params.searchQuery, data]);
+
+    useEffect(() => {
+        setFilteredData(data.filter(call =>
+            call.title.toLowerCase().includes(params.searchQuery.toLowerCase())
+        ));
+    }, [params.searchQuery, data]);
+
+    const handleFilterChange = (newFilter) => {
+        setFilter(newFilter);
+    };
+
+    const results = filteredData.map(call => (
         <SearchResult
             key={call.id}
             data={call}
@@ -24,6 +48,23 @@ export default function SearchPage() {
             <p className='search-page__search-query'>
                 Mostrando {results.length} resultados para "{params.searchQuery}"
             </p>
+            <div className='filter-buttons'>
+                {filters.map((filterType, index) => (
+                    <button
+                        key={index}
+                        onClick={() => handleFilterChange(filterType)}
+                        className={filter === filterType ? 'active' : ''}
+                    >
+                        {filterType}
+                    </button>
+                ))}
+                <button
+                    onClick={() => handleFilterChange('')}
+                    className={filter === '' ? 'active' : ''}
+                >
+                    Todos
+                </button>
+            </div>
             <section className='search-page__results'>
                 {results}
             </section>
