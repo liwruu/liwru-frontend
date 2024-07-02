30ed72e45c8ceceac235f153d82437ee0b0d72b0
Oscar La Torre <oscarlatorre02@gmail.com>
2024-07-01 20:35:37 -0500

    cambios en Info Usuario para fetchear y mostrar reservas

--- src/scenes/infoUsuarioPage/InfoUsuarioPage.jsx
+++ src/scenes/infoUsuarioPage/InfoUsuarioPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from "../../api/axios"
 import { Link } from 'react-router-dom';
 import detail from '../../assets/data/detail';
 import Navbar from '../../components/Navbar/Navbar';  
@@ -10,6 +11,9 @@ const InfoUsuarioPage = () => {
         codigo: '',
         tieneLibroPrestado: false
     });
+    const [reservas, setReservas] = useState([]);
+    const [reservasActivas, setReservasActivas] = useState([]);
+    const [reservasPasadas, setReservasPasadas] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState("all");
 
     const handleCategoryClick = (category) => {
@@ -33,15 +37,40 @@ const InfoUsuarioPage = () => {
             } catch (error) {
                 console.log('An error occurred: ' + error);
             }
-        }
+        };
+        async function fetchUserLoans(){
+            try {
+                const response = await axios.get(`/loans/${usuario.id}`)
+                const data = response.data;
+                setReservas(data)
+                clasificarReservas = (data)
+            } catch (error) {
+                console.error("Error al obtener datos del API", error);
+            }
+        };
 
         fetchUserSession();
+        fetchUserLoans();
     }, []);
 
-    const products = detail[1]; 
+    const clasificarReservas = (reservas) => {
+        const fechaActual = new Date();
+        const activas = [];
+        const pasadas = [];
 
-    const filteredProducts = selectedCategory === "all" ? products : products.filter(product => product.category === selectedCategory);
+        reservas.forEach(reserva => {
+            const returnDate = new Date(reserva.returnDate);
+            if (returnDate >= fechaActual) {
+                activas.push(reserva);
+            } else {
+                pasadas.push(reserva);
+            }
+        });
 
+        setReservasActivas(activas);
+        setReservasPasadas(pasadas);
+    };
+  
     return (
         <div className="page-container">
             <Navbar /> {}
@@ -52,15 +81,40 @@ const InfoUsuarioPage = () => {
                     <p><strong>Código:</strong> {usuario.codigo}</p>
                     <p><strong>Libro prestado:</strong> {usuario.tieneLibroPrestado ? 'Sí' : 'No'}</p>
 
-                    <h2>Mi historial de préstamos</h2>
-                    <section className="products-list">
-                        {filteredProducts.map((product, index) => (
-                            <div className="product-item" key={index}>
-                                <Link to={`/details/${product.id}`} className="product-link">
-                                    <img src={product.image} alt={product.title} className="product-image" />
-                                </Link>
+                    <h2>Mis Prestamos Activos</h2>
+                    <section className="reservas-activas">
+                        {reservasActivas.length > 0 ? (
+                        reservasActivas.map(reserva => (
+                            <div key={reserva.ID}>
+                                <p>ID: {reserva.ID}</p>
+                                <p>Book ID:{reserva.bibliographicMaterialId}</p>
+                                <p>User ID:{reserva.userID}</p>
+                                <p>Loan Date: {reserva.loanDate}</p>
+                                <p>Return Date: {reserva.returnDate}</p>
+                                <button onClick={() => handleExtendReservation(reserva.ID)}>
+                                    Extender Reserva
+                                </button>
                             </div>
-                        ))}
+                        ))
+                        ) : (
+                            <p>No hay reservas activas.</p>
+                        )}
+                    </section>
+                    <h2>Mis Prestamos Anteriores</h2>
+                    <section className = "reservas-pasadas">
+                        {reservasPasadas.length > 0 ? (
+                        reservasPasadas.map(reserva => (
+                            <div key={reserva.ID}>
+                                <p>ID: {reserva.ID}</p>
+                                <p>Book ID:{reserva.bibliographicMaterialId}</p>
+                                <p>User ID:{reserva.userID}</p>
+                                <p>Loan Date: {reserva.loanDate}</p>
+                                <p>Return Date: {reserva.returnDate}</p>
+                            </div>
+                        ))
+                        ) : (
+                            <p>No hay reservas pasadas.</p>
+                        )}
                     </section>
                 </div>
             </div>
