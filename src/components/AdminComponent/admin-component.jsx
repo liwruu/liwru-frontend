cee3861d3bbc486d77256398498c0454b79ba5f6
wifituga <manuel.nazaret.zambrano.moran@gmail.com>
2024-07-17 18:58:20 -0500

    .

--- src/components/AdminComponent/admin-component.jsx
+++ src/components/AdminComponent/admin-component.jsx
@@ -5,7 +5,7 @@ export default function AdminComponent(){
         <div className="admin-container">
             <ul className='admin-component-list'>
                 <li className='admin-list'><a href="/configUser">My information</a></li>
-                <li className='admin-list'><a href="/configUser">My loans</a></li>
+                <li className='admin-list'><a href="/infoUsuario">My loans</a></li>
             </ul>
         </div>
     );
